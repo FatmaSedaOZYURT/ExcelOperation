0b413283cbeb4e9d82d54f5398364a6e1fc5d291
Fatma Seda <fsozyurt@hotmail.com>
2022-04-04 09:03:42 +0300

    Excel Operations

---
+++ ExcelReaderEx/Program.cs
@@ -0,0 +1,50 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReaderEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\file.xlsx";
+
+            FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+
+            IExcelDataReader excelReader;
+            List<FileData> fileDatas = new List<FileData>();
+            int counter = 0;
+
+            if (Path.GetExtension(filePath).ToUpper() == ".XLS")
+            {
+                //Reading from a binary Excel file ('97-2003 format; *.xls)
+                excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+            }
+            else
+            {
+                //Reading from a OpenXml Excel file (2007 format; *.xlsx)
+                excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            }
+
+
+            //Veriler okunmaya başlıyor.
+            while (excelReader.Read())
+            {
+                counter++;
+
+                //ilk satır başlık olduğu için 2.satırdan okumaya başlıyorum.
+                if (counter > 1)
+                {
+                    fileDatas.Add(new FileData() { Title = excelReader.GetValue(0).ToString(), Description = excelReader.GetValue(1).ToString() });
+                }
+            }
+            // Okuma bitiriliyor.
+            excelReader.Close();
+        }
+    }
+}
---
+++ ExcelWriteEx/Program.cs
@@ -0,0 +1,40 @@
+﻿using ExcelReaderEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelWriteEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            List<FileData> fileDatas = new List<FileData>(); //Dolu geldiğini düşünürsek.
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+
+            if (app != null)
+            {
+                Microsoft.Office.Interop.Excel.Workbook myexcelWorkbook = app.Workbooks.Add();
+                Microsoft.Office.Interop.Excel.Worksheet myexcelWorksheet = (Microsoft.Office.Interop.Excel.Worksheet)myexcelWorkbook.Sheets.Add();
+
+                myexcelWorksheet.Cells[1, 1] = "Başlık";
+                myexcelWorksheet.Cells[1, 2] = "Açıklama";
+
+                foreach (var item in fileDatas)
+                {
+                    i++;
+                    myexcelWorksheet.Cells[i, 1] = item.Title;
+                    myexcelWorksheet.Cells[i, 2] = item.Description;
+                }
+
+                app.ActiveWorkbook.SaveAs(@"C:\Users\Public\abc.xlsx", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal);
+
+                myexcelWorkbook.Close();
+                app.Quit();
+            }
+        }
+    }
+}
